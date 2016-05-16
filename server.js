4498cd21a23281677f5e4e27303d303d37a521d9
Ngoc Don <kaitoukid2701@gmail.com>
2016-05-16 16:42:23 +0700

    Done

--- server.js
+++ server.js
@@ -1,9 +1,26 @@
 var express = require("express");
+var url = require('url');
 
 var app = express();
 
-app.get("/", function (req, res) {
-	res.end("hehe");
+app.get("/:str", function (req, res) {
+	// var pURL = url.parse(req.url, true);
+	var result = {};
+	var str = req.params.str;
+	if (/^[0-9]+$/.test(str) == true){
+		str = parseInt(str);
+	}
+	var date = new Date(str);
+	if (date.getTime()){
+		result.unix = date.getTime();
+		result.natural = date.toDateString();
+	}
+	else{
+		result.unix = null;
+		result.natural = null;
+	}
+	res.json(result);
+	res.end();
 });
 
 app.listen(process.env.PORT || 3000);
